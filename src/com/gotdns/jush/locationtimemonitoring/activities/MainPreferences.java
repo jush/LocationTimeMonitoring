53b178f58268bbc5d379cdc89beabbfb4229706e
jush <oni.pool@gmail.com>
2011-04-03 18:27:51 +0300

    Added README and LICENSE files. Added license header to every file.

--- src/com/gotdns/jush/locationtimemonitoring/activities/MainPreferences.java
+++ src/com/gotdns/jush/locationtimemonitoring/activities/MainPreferences.java
@@ -1,3 +1,23 @@
+/**
+   Copyright: 2011 Ramon Sadornil (jush)
+
+   This file is part of LocationTimeMonitoring.
+
+   LocationTimeMonitoring is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   LocationTimeMonitoring is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with LocationTimeMonitoring; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
 package com.gotdns.jush.locationtimemonitoring.activities;
 
 import com.gotdns.jush.locationtimemonitoring.R;
